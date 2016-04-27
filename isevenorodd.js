30fc3fa63c38ec315a1b97afcbe6de82c6da96d8
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-04-27 11:11:59 -0500

    add comments to isevenorodd.js

--- isevenorodd.js
+++ isevenorodd.js
@@ -7,10 +7,10 @@ function isEven(number){
   else if (number === 1) {
     return false;
   }
-  else if (number < 0){
-    return false;
-  }
   else {
-   return isEven(number = number - 2);
+   return isEven(number = number - 2); //isEven calls itself until number is 0
   } 
 }
+
+console.log(isEven(7));
+//false

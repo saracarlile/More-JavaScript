742f4c4c758f4687ecc2c1e252c24c63493a55a4
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-04-29 14:34:43 -0500

    add recursive deepEqual

--- deepequal.js
+++ deepequal.js
@@ -1,3 +1,23 @@
+
+
+function deepEqual(oneThing, twoThing) {
+  if (oneThing === twoThing) return true;
+
+  if (oneThing === null || twoThing === null || typeof oneThing != 'object' || typeof twoThing != 'object') return false;
+
+  var props1 = 0, props2 = 0;
+  for (var prop in oneThing)
+    props1 += 1;
+
+  for (var prop in twoThing) {
+    props2 += 1;
+    if (!(prop in oneThing) || !deepEqual(oneThing[prop], twoThing[prop]))
+      return false;
+  }
+  return props1 === props2;
+}
+
+
 var fishSammy = {
   scales: "pink",
   vegetarian: true
@@ -7,15 +27,8 @@ var fishSally = {
   scales: "pink",
   vegetarian: true
 };
-
-function deepEqual(oneThing, twoThing) {
-  if (oneThing === twoThing) return true;
-  
-  if (oneThing === null || twoThing === null || typeof oneThing != 'object' || typeof twoThing != 'object') return false;
-  
-  var props1 = 0, props2 = 0;
-  
-  
-}
 console.log(deepEqual(fishSammy, fishSally));
 console.log(deepEqual("string", fishSally));
+
+//true
+//false

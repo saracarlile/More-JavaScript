72bb81c79c22fe4fe830cd92d3df3d1cb36051ec
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-04-29 11:02:03 -0500

    array reverse exercise

---
+++ arrayreverse.js
@@ -0,0 +1,8 @@
+var letters = ["A", "B", "C"];
+function reverseArray(someArray){
+  var newArray = [];
+  for(i=someArray.length-1; i>-1; i--){
+    newArray.push(someArray[i]);
+  }
+  return newArray;
+}

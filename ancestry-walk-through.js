1faa011ef821e9a93afdd89c058fb35cc0ba173b
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-05-03 15:04:36 -0500

    ancestry walk through

---
+++ ancestry-walk-through.js
@@ -0,0 +1,8 @@
+var ANCESTRY_FILE = "[\n  " + [
+  '{"name": "Jan Haverbeke", "sex": "m", "born": 1671, "died": 1731, "father": "Lieven Haverbeke", "mother": "Elisabeth Hercke"}',
+  '{"name": "Anna van Hecke", "sex": "f", "born": 1607, "died": 1670, "father": "Paschasius van Hecke", "mother": "Martijntken Beelaert"}',
+  '{"name": "Maria Sturm", "sex": "f", "born": 1835, "died": 1917, "father": "Charles Sturm", "mother": "Seraphina Spelier"}',
+  '{"name": "Jacobus Bernardus van Brussel", "sex": "m", "born": 1736, "died": 1809, "father": "Jan van Brussel", "mother": "Elisabeth Haverbeke"}'
+].join(",\n  ") + "\n]";
+var ancestry = JSON.parse(ANCESTRY_FILE);
+console.log(ancestry);

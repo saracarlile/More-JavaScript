f7d98f98cd72a1683f69eef06c271cd1d8d2256b
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-05-11 09:35:35 -0500

    added information on suggested solution

--- 5-4-everythensome.js
+++ 5-4-everythensome.js
@@ -1,6 +1,8 @@
 //Write two functions, every and some, that behave like these methods (&& and ||) 
 //except that they take the array as their first argument rather than being a method.
 
+//The solution I came up with involved using the built-in some and every array methods.
+
 function nanTest(element, index, array) {
   return isNaN(element);  
 }
@@ -12,3 +14,30 @@ console.log([NaN, 3, 4].some(nanTest));
 //true
 console.log([2, 3, 4].some(nanTest));
 //false
+
+//Eloquent JS solution creates two functions like instructions
+
+function every(array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if (!predicate(array[i]))
+      return false;
+  }
+  return true;
+}
+
+function some(array, predicate) {
+  for (var i = 0; i < array.length; i++) {
+    if (predicate(array[i]))
+      return true;
+  }
+  return false;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
+// → false

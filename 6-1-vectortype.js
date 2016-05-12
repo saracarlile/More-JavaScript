ac8f9a52a8d255ff58b90274bdad21c7be1629d8
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-05-12 10:45:24 -0500

    add length of vector formula

--- 6-1-vectortype.js
+++ 6-1-vectortype.js
@@ -2,7 +2,9 @@
 //Give the Vector prototype two methods, plus and minus, that take another vector as a parameter and 
 //return a new vector that has the sum or difference of the two vectors’ (the one in this and the parameter) x and y values.
 //Add a getter property length to the prototype that computes the length of the vector—that is, 
-//the distance of the point (x, y) from the origin (0, 0).
+//the distance of the point (x, y) from the origin (0, 0).  \
+//length of vector is  equal to  
+//length (x, y)  =    √( x2 + y2 )  http://chortle.ccsu.edu/vectorlessons/vch04/vch04_4.html
 
 function Vector(x, y){
     this.x = x;

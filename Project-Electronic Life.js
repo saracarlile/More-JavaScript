311515290681ee9ba047b8653a1f5c675ca1dd4a
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-05-13 12:57:33 -0500

    initial commit electronic life project

---
+++ Project-Electronic Life.js
@@ -0,0 +1,76 @@
+// http://www.mathopenref.com/coordsquare.html
+//http://www.coolmath.com/reference/rectangles
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+function Grid(width, height) {
+  this.space = new Array(width * height);
+  this.width = width;
+  this.height = height;
+}
+
+Grid.prototype.isInside = function(vector) {
+  return vector.x >= 0 && vector.x < this.width &&
+         vector.y >= 0 && vector.y < this.height;
+};
+Grid.prototype.get = function(vector) {
+  return console.log(this.space[vector.x + this.width * vector.y]);
+};
+Grid.prototype.set = function(vector, value) {
+  this.space[vector.x + this.width * vector.y] = value;
+}; 
+//testing
+//var something = new Grid(4,5);
+//console.log(something.space);
+//var vector = new Vector(2, 2);
+//something.set(vector, "x");
+//console.log(something.space);
+
+var directions = {
+  "n":  new Vector( 0, -1),
+  "ne": new Vector( 1, -1),
+  "e":  new Vector( 1,  0),
+  "se": new Vector( 1,  1),
+  "s":  new Vector( 0,  1),
+  "sw": new Vector(-1,  1),
+  "w":  new Vector(-1,  0),
+  "nw": new Vector(-1, -1)
+};
+
+function randomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+var directionNames = "n ne e se s sw w nw".split(" ");
+
+function BouncingCritter() {
+  this.direction = randomElement(directionNames);
+}
+
+BouncingCritter.prototype.act = function(view) {
+  if (view.look(this.direction) != " ")
+    this.direction = view.find(" ") || "s";
+  return {type: "move", direction: this.direction};
+};
+
+function elementFromChar(legend, ch) {
+  if (ch == " ")
+    return null;
+  var element = new legend[ch]();
+  element.originChar = ch;
+  return element;
+}
+
+function World(map, legend) {
+  var grid = new Grid(map[0].length, map.length);
+  this.grid = grid;
+  this.legend = legend;
+
+  map.forEach(function(line, y) {
+    for (var x = 0; x < line.length; x++)
+      grid.set(new Vector(x, y),
+               elementFromChar(legend, line[x]));
+  });
+}

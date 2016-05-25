ecbb94c146f37950f9db91c21bb732d2302550be
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-05-25 14:00:45 -0500

    reg expressions

--- 9-1-regexpgolf.js
+++ 9-1-regexpgolf.js
@@ -16,3 +16,13 @@ console.log(ious.test("delicious"));
 //true
 //5. A whitespace character followed by a dot, comma, colon, or semicolon
 var dotCommaSemicolon = /\s[.;:]/;
+console.log(dotCommaSemicolon.test("something ."));
+//true
+//6. A word longer than six letters
+var moreThanSix = /\w{7,}/;
+console.log(moreThanSix.test("something"));
+//true
+//7. A word without the letter e
+var noE = /\b[a-df-z]+\b/i;
+console.log(noE.test("Blond"));
+//true

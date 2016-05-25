869e3640da8613eaa71bee9f0201e77149c3c461
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-05-25 13:52:35 -0500

    modified reg exercise

--- 9-1-regexpgolf.js
+++ 9-1-regexpgolf.js
@@ -1,23 +1,18 @@
 //1. car and cat
-var carCat = /ca( r|t)/;
-console.log(carCat.test("cat"));
+var carCat = /ca(r|t)/;
+console.log(carCat.test("tomcat"));
 //true
 //2. pop and prop
 var popProp = /pr?op/;
-console.log(popProp.test("prop"));
+console.log(popProp.test("proposal"));
 //true
 //3. ferret, ferry, and ferrari
-var fer = /fer+(et|y|ari)/;
+var fer = /ferr(et|y|ari)/;
 console.log(fer.test("ferrari"));
 //true
 //4. Any word ending in ious
-var word = "delicious";
-console.log(word.search(/ious\b/));
-//5
-function search(word){
-  if (word.search(/ious\b/) > -1)
-    return true;
-}
-console.log(search(word));
+var ious = /ious\b/;
+console.log(ious.test("delicious"));
 //true
 //5. A whitespace character followed by a dot, comma, colon, or semicolon
+var dotCommaSemicolon = /\s[.;:]/;

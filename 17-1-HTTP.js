40e688cd1d9a74772d98991afe2c74b4460b4f04
Sara Carlile <sara.elizabeth.carlile@gmail.com>
2016-07-01 13:12:41 -0500

    HTTP exercise edits

--- 17-1-HTTP.js
+++ 17-1-HTTP.js
@@ -1,11 +1,24 @@
 function request(type){
-    req.open("GET", "http://eloquentjavascript.net/author", true;
+    var req = new XMLHttpRequest();
+    req.open("GET", "http://eloquentjavascript.net/author", false);
     req.setRequestHeader("Accept", type); 
-    req.addEventListener("load", function() {
-        console.log(req.status);
-    });
     req.send(null);
+    return req.responseText;
 }
 
+var types = ["text/html","application/json", "text/plain", "application/rainbows+unicorns" ];
 
-var types = ["text/html","application/json", "text/plain" ];
+types.forEach(function(type){
+    try {
+    console.log(type + ":\n", request(type), "\n");
+  } catch (e) {
+    console.log("Raised error: " + e);
+  }
+});
+
+//example output
+//"text/plain:
+//"
+//"My name is Marijn Haverbeke. You can email me at marijn@haverbeke.nl, or visit my website, http://marijnhaverbeke.nl .
+//"
+//"
